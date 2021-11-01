0b33909442e34cadab9f3f2f6d7e10abfbf43950
crescentsoru <smashjojoff@gmail.com>
2021-11-01 15:18:52 +0300

    cleanup, what the fuck is GAP

--- Base/Stage/Camera.gd
+++ Base/Stage/Camera.gd
@@ -1,14 +1,10 @@
 extends Camera2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 
--- Base/pECB.gd
+++ Base/pECB.gd
@@ -1,13 +1,10 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 var collisions = []
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	connect( "body_entered", self, "on_body_enter")
 	connect( "body_exited", self, "on_body_exit")
@@ -26,6 +23,6 @@ func on_body_exit(body):
 func on_area_enter(area): #or this
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _process(delta):
+	pass
